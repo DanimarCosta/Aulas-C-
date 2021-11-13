32b574ac7f79892f9b3d1a5cfc84b01b33d3ee34
Danimar Campos da Costa <72043658+DanimarCosta@users.noreply.github.com>
2021-11-12 23:08:13 -0300

    Create music_player.c
    Reprodutor de música com o buzzer no Arduino

---
+++ music_player.c
@@ -0,0 +1,35 @@
+//for public use and alteration.  Enjoy
+  int bellPin = 9;
+  
+  void setup(){
+    pinMode(bellPin, OUTPUT);
+  }
+  void loop(){
+    digitalWrite(bellPin, HIGH);
+  
+  tone(bellPin, 988, 250);
+  delay(1000);
+  tone(bellPin, 988, 250);
+  delay(400);
+  tone(bellPin, 880, 250);
+  delay(400);
+  tone(bellPin, 988, 250);
+  delay(1000);
+  
+  tone(bellPin, 988, 250);
+  delay(400);
+  tone(bellPin, 880, 250);
+  delay(400);
+  tone(bellPin, 988, 250);
+  delay(1000);
+  
+  tone(bellPin, 988, 250);
+  delay(400);
+  tone(bellPin, 880, 250);
+  delay(400);
+  tone(bellPin, 784, 1000);
+  delay(1000);
+  tone(bellPin, 880, 1000);
+  delay(1000);
+  //replays loop
+  }
